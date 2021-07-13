f84e4af78f25dd14a0a729f19aee5ff2c2ab0193
Vitaly-48 <73639081+Vitaly-48@users.noreply.github.com>
2021-07-13 17:40:39 +0300

    Create My_Map_Bot.py

---
+++ My_Map_Bot/My_Map_Bot.py
@@ -0,0 +1,14 @@
+import telebot
+import json
+
+
+token = '1897202859:AAG0mNSPsMQ446m4WQ9IfnwQj3ZHamoUuV8'
+
+name = input("Напечатай текст для разворота: ")
+example_string = name
+print(f"Результат: {example_string[::-1]}")
+   
+
+
+if __name__ == '__main__':
+  bot.polling()
